e176a996a989363a4e3a3726b616621aaf091360
ramil0712 <rami-l@mail.ru>
2026-01-21 21:24:52 +0400

    added frontend folder and add post func

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form
 from pydantic import BaseModel
 from typing import  Optional, List, Dict
+
+
+class User(BaseModel):
+    id:int
+    name: str
+    age: int
 
 
 class Post(BaseModel):
     id: int
     title: str
     body: str
+    author: User
 
+
+class PostCreate(BaseModel):
+    title: str
+    body: str
+    author_id:int
+
+
+
+users = [
+    {"id": 1, "name": "John", "age": 25},
+    {"id": 2, "name": "Mike", "age": 35},
+    {"id": 3, "name": "Flinn", "age": 45}
+]
 
 posts = [
-    {"id":1, "title":"Post 1", "body":"This is a body text of the Post 1"},
-    {"id":2, "title":"Post 2", "body":"This is a body text of the Post 2"},
-    {"id":3, "title":"Post 3", "body":"This is a body text of the Post 3"}
+    {"id": 1, "title": "Post 1", "body": "This is a body text of the Post 1","author": users[1]},
+    {"id": 2, "title": "Post 2", "body": "This is a body text of the Post 2","author": users[0]},
+    {"id": 3, "title": "Post 3", "body": "This is a body text of the Post 3","author": users[2]}
 ]
 
 
 app = FastAPI()
+
+@app.get("/")
+async def index():
+    return "This is a homepage"
 
 
 @app.get("/items")
@@ -31,6 +55,16 @@ async def items(id: int) -> Dict:
             return Post(**post)
     
     raise HTTPException(status_code=404, detail="The post number {id} is not available")
+
+@app.post("/items/add")
+async def add_item(post: PostCreate) -> Post:
+    author = next((user for user in users if user["id"] == post.author_id) , None)
+    if not author:
+        raise HTTPException(status_code=404, detail='User not found')
+    new_post_id = len(posts) + 1
+    new_post = {"id": new_post_id, "title": post.title, "body": post.body, "author": author}
+    posts.append(new_post)
+    return Post(**new_post)
 
 
 @app.get("/search")
